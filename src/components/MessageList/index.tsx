af584b3669f90468f301015bc33195f8a1a64487
LeoRangel <leandro.mdrs06@gmail.com>
2021-10-22 21:13:16 -0300

    Configurar mensagens em tempo real no componente MessageList usando socket.io-client

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { api } from "../../services/api"
+import { api } from "../../services/api";
+import { io } from "socket.io-client";
 
 import styles from './styles.module.scss';
 
@@ -12,9 +13,33 @@ type Message = {
     }
 }
 
+// Lista de mensagens
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+// Add nova mensagem na lista
+socket.on('new_message', (newMessage: Message) => {
+    messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
 
     const [messages, setMessages] = useState<Message[]>([]);
+
+    useEffect(() => {
+        setInterval(() => {
+            if (messagesQueue.length > 0) {
+                setMessages(prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean))
+
+                messagesQueue.shift();
+            }
+        }, 3000)
+    }, [])
 
     useEffect(() => {
         api.get<Message[]>('messages/last3').then(response => {
